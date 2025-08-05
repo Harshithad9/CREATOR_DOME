04f305db759cf42a2c1ee73bd182af5a81aa9d95
Harshitha D <dharshitha292@gmail.com>
2025-08-05 21:21:19 +0530

    Delete routes directory

--- routes/promptRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/promptRoutes.js
-const express = require("express");
-const router = express.Router();
-const verifyFirebaseToken = require("../middleware/authMiddleware");
-const { savePrompt } = require("../controllers/promptController");
-
-router.post("/save", verifyFirebaseToken, savePrompt);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require("express");
-const router = express.Router();
-
-router.get("/test", (req, res) => {
-  res.send("👋 Backend route working!");
-});
-
-module.exports = router;
