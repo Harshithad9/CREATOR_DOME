dd0f211bfb22abf6bec82821d8ce165061685e26
Harshitha D <dharshitha292@gmail.com>
2025-08-10 22:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+
+  
   console.log(`🚀 Server running on port ${PORT}`);
 });
